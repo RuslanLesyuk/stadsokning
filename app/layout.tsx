7f491db1ea9fbcc883d746256e1dc533a5aa5ff2
owico <lesyukruslan@gmail.com>
2026-06-07 12:38:01 +0200

    Polish mobile dashboard and job details pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -257,7 +257,22 @@ export default async function RootLayout({
             })(window, document, "clarity", "script", "wzu4anu3qc");
           `}
         </Script>
+              <Script
+  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+  strategy="afterInteractive"
+/>
 
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+      page_path: window.location.pathname,
+    });
+  `}
+</Script>
         <Analytics />
       </body>
     </html>
