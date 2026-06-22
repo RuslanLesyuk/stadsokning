2f84f1a511bf724582e614271086b602c3f64f3f
owico <lesyukruslan@gmail.com>
2026-06-22 20:02:41 +0200

    button delete

--- components/delete-job-form.tsx
+++ components/delete-job-form.tsx
@@ -106,7 +106,7 @@ export default function DeleteJobForm({ jobId }: { jobId: string }) {
         onClick={() => setIsOpen(true)}
         className="inline-flex items-center justify-center rounded-xl border border-red-200 bg-red-50 px-4 py-2 text-sm font-medium text-red-700 transition hover:bg-red-100"
       >
-        DELETE TEST 123
+        {t.delete}
       </button>
       
 
