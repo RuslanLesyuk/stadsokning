65c38e8981b6cc7fc61b93f409389865a7ff7081
owico <lesyukruslan@gmail.com>
2026-06-22 19:57:26 +0200

    Polish mobile dashboard and job details pages

--- components/delete-job-form.tsx
+++ components/delete-job-form.tsx
@@ -106,11 +106,9 @@ export default function DeleteJobForm({ jobId }: { jobId: string }) {
         onClick={() => setIsOpen(true)}
         className="inline-flex items-center justify-center rounded-xl border border-red-200 bg-red-50 px-4 py-2 text-sm font-medium text-red-700 transition hover:bg-red-100"
       >
-        {t.delete}
+        DELETE TEST 123
       </button>
-      <button type="button" onClick={() => alert("TEST")}>
-  TEST1
-</button>
+      
 
       {isOpen ? (
         <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/40 px-4">
